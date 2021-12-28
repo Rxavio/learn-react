8da216c4a87b2278e45c3782694547dd69fc4111
xavioo <rucahoxavi@gmail.com>
2021-12-28 07:55:12 +0200

    setState increament

--- react-from-zero/src/App.js
+++ react-from-zero/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import './App.css'
 //  import Greet from './components/Greet'
 //  import Welcome from './components/Welcome'
- import Message from './components/Message'
+//  import Message from './components/Message'
 //import Hello from './components/Hello'
+import Count from './components/Counter'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <br />
+        <Count />
 {/*         
         <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
@@ -24,7 +26,7 @@ class App extends Component {
         <Welcome name="Clark" heroName="Superman" />
         <Welcome name="Diana" heroName="Wonder Woman" /> */}
 
-        <Message message='Welcome Visitor' />
+        {/* <Message message='Welcome Visitor' /> */}
 
         {/* <Greet />
         <Welcome />
