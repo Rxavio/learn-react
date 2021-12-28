b19b85005fbb24ea07a661b88fa4af4ae5a2b044
xavioo <rucahoxavi@gmail.com>
2021-12-28 08:13:01 +0200

    add click event handler in a classn

--- react-from-zero/src/App.js
+++ react-from-zero/src/App.js
@@ -5,13 +5,15 @@ import './App.css'
 //  import Message from './components/Message'
 //import Hello from './components/Hello'
 // import Count from './components/Counter'
-import FunctionClick from './components/FunctionClick'
+// import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <br />
-        <FunctionClick />
+        <ClassClick />
+        {/* <FunctionClick /> */}
         {/* <Count /> */}
 {/*         
         <Greet name="Bruce" heroName="Batman">
