aec59f8a6a4569dfdab14f20331a3218d4412e5e
xavioo <rucahoxavi@gmail.com>
2021-12-29 20:03:14 +0200

    list rendering with map method

--- react-from-zero/src/components/NameList.js
+++ react-from-zero/src/components/NameList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+  const names = ['Bruce', 'Clark', 'Diana', 'Rico']
  return(
      <div>
-       <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2>
+      {
+       names.map(name => <h2>{name}</h2>)   
+      }
     </div>
  )
 }
