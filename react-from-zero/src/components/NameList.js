0974d7c051914148af2b3724c78d3a0863ae3137
xavioo <rucahoxavi@gmail.com>
2021-12-29 20:05:23 +0200

    list rendering with map method 2

--- react-from-zero/src/components/NameList.js
+++ react-from-zero/src/components/NameList.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
+// function NameList() {
+//   const names = ['Bruce', 'Clark', 'Diana', 'Rico']
+//  return(
+//      <div>
+//       {
+//        names.map(name => <h2>{name}</h2>)   
+//       }
+//     </div>
+//  )
+// }
+
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana', 'Rico']
- return(
-     <div>
-      {
-       names.map(name => <h2>{name}</h2>)   
-      }
-    </div>
- )
-}
+    const names = ['Bruce', 'Clark', 'Diana', 'Rico']
+    const nameList=  names.map(name => <h2>{name}</h2>)
+   return(
+       <div>
+        {
+        nameList  
+        }
+      </div>
+   )
+  }
 
 export default NameList
